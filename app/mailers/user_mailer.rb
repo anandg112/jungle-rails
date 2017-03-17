5a8535275b11ca168869ac09b7af231f6caf3333
Anand <anandg112@gmail.com>
2017-03-17 00:10:22 +0000

    added mailer on console

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,9 @@
 class UserMailer < ApplicationMailer
   default from: 'no-reply@jungle.com'
 
-  def welcome_email(user)
-    @user = user
-    @url = '/login'
-    mail(to: @user.email, subject: "Welcome")
-  end
-
   def order_email(order)
     @order = order
     mail(to: @order.email, subject: "Your Order #{@order.id}")
   end
+  
+end
