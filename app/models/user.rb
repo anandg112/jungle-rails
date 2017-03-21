b28dc1e6cd40fa311ae4e8a32dec51bf70880f35
Anand <anandg112@gmail.com>
2017-03-21 23:50:27 +0000

    added email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :reviews
-  # validates :password, presence: true
-  # validates :email, presence: true, uniqueness: {case_sensitive: false}
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
+  validates :password, presence: true
+  validates :email, presence: true, uniqueness: true, case_sensitive: false
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
